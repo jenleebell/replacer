3ccf548a0de072d13dec7381578a709a8adf600c
Jennifer <jenleebell@gmail.com>
2015-09-11 11:43:04 -0700

    Update form function with styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 function findReplace(sentence, originalWord, replacedWord) {
   return sentence.replace(new RegExp(originalWord, 'gi'), replacedWord);
 }
+
+$(document).ready(function() {
+  $("form#findReplace").submit(function(event) {
+    var sentence = $("input#sentence").val();
+    var originalWord = $("input#originalword").val();
+    var replacedWord = $("input#replacedword").val();
+    var result = findReplace(sentence, originalWord, replacedWord);
+
+    $("#result p").text(result);
+    $("#result").show();
+
+    event.preventDefault();
+  })
+})
