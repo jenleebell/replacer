66422692e4c5d2f2981cb9d5ec74ced68aa2f715
Jennifer <jenleebell@gmail.com>
2015-09-11 10:03:49 -0700

    Add code to replace each instance of the new word in a sentence

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
 function findReplace(sentence, originalWord, replacedWord) {
-  return originalWord.replace(originalWord, replacedWord)
-};
+  return sentence.replace(new RegExp(originalWord, 'g'), replacedWord);
+}
--- spec/specs.js
+++ spec/specs.js
@@ -5,7 +5,11 @@ describe("findReplace", function() {
   })
 
   it("finds multiple instances of a word in a sentence and replaces them all with the new word", function(){
-    expect(findReplace("the dog says meow meow", "meow", "woof")).to.eql("woof")
+    expect(findReplace("the dog says meow meow", "meow", "woof")).to.eql("the dog says woof woof")
   })
+
+  // it("finds multiple instances of a word in a sentence and replaces them all with the new word regardless of the case", function() {
+  //   expect(findReplace("Mr. Meow the dog says meow meow", "meow", "woof")).to.eql("woof")
+  // })
 
 })
