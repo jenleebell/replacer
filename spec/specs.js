bdc4bda5a73575dea828256815afc27d87cf3687
Jennifer <jenleebell@gmail.com>
2015-09-11 09:43:13 -0700

    Update formatting errors in second spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,11 @@
-describe("replacer", function() {
+describe("findReplace", function() {
 
   it("finds one word in a sentence and replaces it with a new word", function(){
     expect(findReplace("meow", "meow", "woof")).to.eql("woof")
   })
 
-  it("finds multiple instances of a word in a sentence and replaces them all with the new word")
+  it("finds multiple instances of a word in a sentence and replaces them all with the new word", function(){
     expect(findReplace("the dog says meow meow", "meow", "woof")).to.eql("woof")
+  })
 
 })
